e5f140549259d4065107fc0326dbda0bcb3fb3ea
NEU-KKristof <karanyiczkristof98@gmail.com>
2025-05-19 23:21:20 +0200

    Update user_auth_db.sql

--- user_auth_db.sql
+++ user_auth_db.sql
@@ -1,33 +1,10 @@
 CREATE DATABASE user_auth_db;
 
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 05, 2025 at 02:25 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+/*! Válaszd ki az adatbázist*/
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `user_auth_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -37,37 +14,14 @@ CREATE TABLE `users` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `users`
---
-
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `created_at`) VALUES
 (1, 'Admin', 'Admin@gmail.com', '$2y$10$m3wtco7AWaNihxsRrRt3iuc2FhVl/.nvCJuTecrpbnPoDkdrKsrN.', '2025-04-05 11:56:23'),
 (2, 'Test', 'Test@gmail.com', '$2y$10$jN/7aVDDiiZ4I5TZM.Wo8enRR9aHH8Wo.82AAYRo.g3R0OiIFvCca', '2025-04-05 11:56:49');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`),
   ADD UNIQUE KEY `email` (`email`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
